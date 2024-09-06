7d5a59f0ee9bf3349ce45abb3b116d64b9ea6cc4
dev-hoale <leconghoa11a1@gmail.com>
2024-09-06 23:15:55 +0700

    m

--- validatons/customer.validation.js
+++ validatons/customer.validation.js
@@ -1,27 +1,29 @@
-const Joi = require('joi');
+const Joi = require("joi");
 const passwordRegex = new RegExp(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/);
 
-const validatePassword = (value) => {  
-    if(!passwordRegex.test(String(value))) { 
-        throw new Error('Password should contains a lowercase, a uppercase character and a digit.')
-    }
-}
+const validatePassword = (value) => {
+  if (!passwordRegex.test(String(value))) {
+    throw new Error(
+      "Password should contains a lowercase, a uppercase character and a digit."
+    );
+  }
+};
 
 module.exports = {
-    create: Joi.object().keys({
-        email: Joi.string().email().required(),
-        full_name: Joi.string().required(),
-        phone: Joi.string().required(),
-        ref_email: Joi.string().email().required(),
-        password: Joi.string().min(8).max(16).required().external(validatePassword)
-    }),
-    update: Joi.object().keys({
-        image: Joi.string().allow("").allow(null),
-        full_name: Joi.string().required(),
-        phone: Joi.string().email().required(),
-    }),
-    login: Joi.object().keys({
-        email: Joi.string().email().required(),
-        password: Joi.string().required()
-    }),
-}
+  create: Joi.object().keys({
+    email: Joi.string().email().required(),
+    full_name: Joi.string().required(),
+    phone: Joi.string().required(),
+    ref_email: Joi.string().email().required(),
+    password: Joi.string().min(8).max(16).required().external(validatePassword),
+  }),
+  update: Joi.object().keys({
+    image: Joi.string().allow("").allow(null),
+    full_name: Joi.string().required(),
+    phone: Joi.string().required(),
+  }),
+  login: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+};
