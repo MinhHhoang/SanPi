6cf69b342eaf2a772b8a85c150f803d9467f8c11
MinhHhoang <70305905+MinhHhoang@users.noreply.github.com>
2024-09-15 19:40:29 +0700

    update

--- validatons/order_coin.validation.js
+++ validatons/order_coin.validation.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 
 module.exports = {
   create: Joi.object().keys({
-    status_order: Joi.string().email().required(),
+    status_order: Joi.string().required(),
     type_order: Joi.string().required(),
     type_coin: Joi.string().required(),
   }),
