8fa72b536872ac16ecffad86a48ad5ba4c3f38d1
MinhHhoang <70305905+MinhHhoang@users.noreply.github.com>
2024-09-16 09:16:41 +0700

    update

--- validatons/order_coin.validation.js
+++ validatons/order_coin.validation.js
@@ -5,8 +5,13 @@ module.exports = {
   create: Joi.object().keys({
     type_order: Joi.string().required(),
     type_coin: Joi.string().required(),
+    price_coin_current :Joi.number().allow(null),
+    stk :Joi.string().allow("").allow(null),
+    stk_bank :Joi.string().allow("").allow(null),
+    stk_name :Joi.string().allow("").allow(null),
+    total_money :Joi.number().allow(null),
+    wallet_coin :Joi.string().allow("").allow(null),
   }),
-  
 };
 
 
